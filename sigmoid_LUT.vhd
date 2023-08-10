e8d12cdaad776db43d0d29ec1dfe5a1d18ec5245
nataliehanekom <123349341+nataliehanekom@users.noreply.github.com>
2023-08-10 22:29:49 +0200

    Add files via upload

--- sigmoid_LUT.vhd
+++ sigmoid_LUT.vhd
@@ -13,14 +13,17 @@ end sigmoid_LUT;
 
 architecture archPrac1 of sigmoid_LUT is
 	signal send_value, temp_send_value, send_LUT, temp_send_LUT, sending, received, prev_received, clear_buf, receive_flag : std_logic := '0';
-	signal value_sent, value_sent_pulse : std_logic := '1';
+	signal prev_value_sent, value_sent, value_sent_pulse : std_logic := '1';
 	signal received_byte, high_byte, low_byte : std_logic_vector(7 downto 0);
 	signal tx_data, temp_tx_data, address : std_logic_vector(15 downto 0) := "0000000000010101"; -- 16 bit values	 
-	signal num_values_sent, temp_num_values_sent, num_vals, adrs : integer range 0 to 65536 := 0; 
+	signal num_values_sent, temp_num_values_sent, num_vals, adrs, LUT_address : integer range 0 to 65536 := 0; 
 	signal rx_counter, num_bytes_received : integer range 0 to 3;
 	type rx_buffer is array (0 to 2) of std_logic_vector(7 downto 0);
    signal receive_buffer : rx_buffer;
 	signal rx_buffer0, rx_buffer1, rx_buffer2 : std_logic_vector(7 downto 0);
+	
+	type state_type is (ST0,ST1,ST2); 
+	signal PS,NS : state_type := ST0;
 	 
 	component UART is
 	port (
@@ -59,57 +62,99 @@ begin
 			 
 		LUT_inst : LUT port map
 			(
-			address => num_values_sent,
+			address => LUT_address,--num_values_sent,
 			data_val => tx_data
 			);
 			 
 			 
 			 
-			 
-		
-	
-		
-		send_value <= temp_send_value or not(value_sent) or send_LUT or sending;
-		
-		
-					  
-		--tx_data <= address;--"0100010001010011";-- loads data		
-		
-		num_values_sent <= temp_num_values_sent when (num_vals = 65536) else
-								 adrs;
-		
-		sending <= '0' when (num_values_sent = 0) else -- continues sending LUT
-					  '1' when (num_values_sent < num_vals and receive_flag = '1') else
-					  '0';
-					  
-		send_LUT <= '1' when (rx_buffer2 = "01010011") else --(received_byte = "01010011" and rx_counter = 0) else  
-						'0';
-						
-		num_vals <= 65536 when (rx_buffer2 = "01010011") else
-						1 when (rx_buffer2 = "01000100");
-						
-	
-		temp_send_value <= '1' when (rx_buffer2 = "01000100") else
-								 '0';						 
-								 
-		
-		address <= (rx_buffer0 & rx_buffer1) when (rx_buffer2 = "01000100");
-		
-		adrs <= to_integer(unsigned(address)) when (rx_buffer2 = "01000100") ;
 
-								 
 		
-		clear_buf <= '1' when value_sent_pulse = '1' else -- clear the receive buffer after transmitting a value so that received command does not execute again 
-						 '0';	
-						 
 		
-		--test <= clear_buf;
+		test <= '1' when PS = ST1 else
+				  '0'; 
 		
-		test <= sending;
 		
-		--num_bytes_received <= rx_counter;
+		sync_proc : process (clk, NS)
+		begin
+			if rising_edge(clk) then
+				PS <= NS;
+			end if;
+		end process sync_proc;
 		
-		rx : process (received)	
+		
+		
+		comb_proc : process (PS, num_values_sent, value_sent, rx_buffer2)
+		begin
+			case PS is
+				when ST0 =>
+				
+					send_value <= '0';
+				
+					if rx_buffer2 = "01010011" then -- received "S"
+						NS <= ST1; -- send LUT
+						
+						send_value <= '1';
+						LUT_address <= 0;
+					elsif rx_buffer2 = "01000100" then -- received "D"
+						NS <= ST2; -- send one value
+						
+						send_value <= '1';
+						address <= (rx_buffer0 & rx_buffer1);		
+						LUT_address <= to_integer(unsigned(address));
+					else
+						NS <= ST0;
+						send_value <= '0';
+						LUT_address <= 0;
+						
+					end if;
+					
+					clear_buf <= '0';
+					
+				when ST1 =>
+				
+					send_value <= '1'; 					
+					
+					LUT_address <= num_values_sent;
+					
+					if (num_values_sent = 65536) then						
+						NS <= ST0;
+						clear_buf <= '0';
+					else 
+						NS <= ST1;
+						clear_buf <= '1';
+					end if;						
+					
+					 
+					
+					
+				when ST2 =>
+				
+					--address <= (rx_buffer0 & rx_buffer1);		
+					--LUT_address <= to_integer(unsigned(address));
+					
+					send_value <= '1';
+					
+					if value_sent = '1' then
+						NS <= ST0;
+						clear_buf <= '0';
+					else
+						NS <= ST2;
+						clear_buf <= '1';
+					end if;	
+		
+					
+					
+				when others =>
+					NS <= ST0;
+			end case;
+		end process comb_proc;
+		
+		
+		
+		
+		
+		rx : process (received, clear_buf)	
 			variable counter : integer range 0 to 3 := 0;
 		begin
 			if clear_buf = '1' then	
@@ -124,7 +169,6 @@ begin
 				rx_buffer1 <= rx_buffer0;
 				rx_buffer0 <= received_byte;
 				
-				--rx_counter <= rx_counter + 1;
 				counter := counter + 1;
 				
 				if counter = 3 then
@@ -138,20 +182,27 @@ begin
 					
 					
 		
+		num_values_sent <= temp_num_values_sent;
 		
 		
-		
-		address_inc : process (value_sent) -- load new data value		
+		address_inc : process (value_sent) -- increment LUT adress
 		begin	
+		
+		if PS = ST1 then
 			
 			if rising_edge(value_sent) then				
-				temp_num_values_sent <= temp_num_values_sent + 1;
 				
-				if temp_num_values_sent = num_vals then
+				if temp_num_values_sent = 65536 then
 					temp_num_values_sent <= 0;
+				else
+					temp_num_values_sent <= temp_num_values_sent + 1;
 				end if;
 						
-			end if; 					
+			end if; 
+		else
+			temp_num_values_sent <= 0;
+			
+		end if;			
 		
 		end process address_inc;
 		
